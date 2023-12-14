fcff046a272b0226b3d21ff8945d222c1d4b83ee
IvanOrsh <ksironov@mail.ru>
2023-12-14 20:35:23 +0300

    feat: scaffold creat entry form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  redirect,
+  type ActionFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +16,14 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const json = Object.fromEntries(formData);
+  console.log("[from action function]:", { json });
+
+  return redirect("/");
+}
+
 export default function Index() {
   return (
     <div className="p-20">
@@ -18,6 +31,70 @@ export default function Index() {
       <p className="mt-2 text-lg text-gray-400">
         Learnings and doings. Updated weekly.
       </p>
+
+      <div className="my-8 border p-2">
+        <Form method="post">
+          <p className="italic">Create an entry.</p>
+
+          {/* category */}
+          <div>
+            <div className="mt-4">
+              <input type="date" name="date" className="text-gray-700" />
+            </div>
+
+            <div className="mt-4 space-x-6">
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="work"
+                />
+                Work
+              </label>
+
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="learning"
+                />
+                Learning
+              </label>
+
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="Interesting-thing"
+                />
+                Interesting things
+              </label>
+            </div>
+
+            {/* text field */}
+            <div className="mt-2">
+              <textarea
+                name="text"
+                className="w-full text-gray-700"
+                placeholder="Write your entry..."
+              />
+            </div>
+
+            {/* submit button */}
+            <div className="mt-1 text-right">
+              <button
+                className="bg-blue-500 px-4 py-1 font-medium text-white "
+                type="submit"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Form>
+      </div>
 
       <div className="mt-4">
         <p className="font-bold">
