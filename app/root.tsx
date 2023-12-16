6ed760358527fbea96b4f4c1b4ec2364a277f5cd
IvanOrsh <ksironov@mail.ru>
2023-12-17 02:43:13 +0300

    fix: improve login form styles

--- app/root.tsx
+++ app/root.tsx
@@ -78,7 +78,7 @@ export default function App() {
               </div>
             </div>
 
-            <div className="mb-20 mt-16">
+            <div className="my-20">
               <div className="text-center">
                 <h1 className="text-5xl font-semibold tracking-tight text-white">
                   <Link to="/">Diaro de Trabajo</Link>
